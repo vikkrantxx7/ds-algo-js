6e76f8ffdb82b843d711729eca6bd6b3259f11d6
Vikrant Sharma <vikrant.sharma@kronos.com>
2021-01-25 12:22:44 +0530

    add contains to bst

--- data structures/binarySearchTree.js
+++ data structures/binarySearchTree.js
@@ -20,6 +20,9 @@ class BinarySearchTree {
             return this
         }
         while (true) {
+            if (val === current.value) {
+                return undefined
+            }
             if (val < current.value) {
                 if (current.left === null) {
                     current.left = node
@@ -38,7 +41,7 @@ class BinarySearchTree {
         }
     }
 
-    search = val => {
+    find = val => {
         let current = this.root
 
         while (true) {
@@ -47,6 +50,26 @@ class BinarySearchTree {
             }
             if (val === current.value) {
                 return current
+            }
+            if (val < current.value) {
+                current = current.left
+            }// by not putting else-if if value is smaller than the greater than check
+            // for next node is checked in the same iteration
+            if (val > current.value) {
+                current = current.right
+            }
+        }
+    }
+
+    contains = val => {
+        let current = this.root
+
+        while (true) {
+            if (current === null) {
+                return false
+            }
+            if (val === current.value) {
+                return true
             }
             if (val < current.value) {
                 current = current.left
