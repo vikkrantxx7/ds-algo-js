a2a055a8dd709fd8fd8dc7961cc8d4826f2a94ab
Vikrant Sharma <vikrant.sharma@kronos.com>
2021-01-27 22:46:12 +0530

    graph

---
+++ data structures/graph.js
@@ -0,0 +1,33 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {}
+    }
+
+    addVertex = vertex => {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = []
+        }
+    }
+
+    addEdge = (vertex_1, vertex_2) => {
+        this.adjacencyList[vertex_1].push(vertex_2)
+        this.adjacencyList[vertex_2].push(vertex_1)
+    }
+
+    removeVertex = vertex => {
+        Object.keys(this.adjacencyList).forEach(key => {
+            if (key === vertex) {
+                return
+            }
+            if (this.adjacencyList[key].includes(vertex)) {
+                this.adjacencyList[key] = this.adjacencyList[key].filter(item => vertex !== item)
+            }
+        });
+        delete this.adjacencyList[vertex]
+    }
+
+    removeEdge = (vertex_1, vertex_2) => {
+        this.adjacencyList[vertex_1] = this.adjacencyList[vertex_1].filter(vertex => vertex !== vertex_2)
+        this.adjacencyList[vertex_2] = this.adjacencyList[vertex_2].filter(vertex => vertex !== vertex_1)
+    }
+}
